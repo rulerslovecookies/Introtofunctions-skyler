cce07e983ae3982c70c57c981edb7611bb5779d4
rulerslovecookies <zhu.skyler@gmail.com>
2026-09-23 07:17:12 -0700

    fgz

--- big.py
+++ big.py
@@ -2,17 +2,22 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-for i in range(60):
+for i in range(4):
     def square(X):
         length = 5
-        def length(length):
-         for i in range(4):
-                    t.forward(length)
-                    t.left(90)
+        
+    
+for i in range(4):
+ t.forward(length)
+ t.left(90)
+length(length)=5
+
     square(100)
     t.right(5)
-    length = +5
+  
+    
+  
    
    
-    print(i)
+    
 turtle.done()
---
+++ notes.py
@@ -0,0 +1,42 @@
+bill=int(input("how much is  the bill amount: "))
+print(bill+20)
+def add (x,y):
+    return x+y
+
+rain= True
+if rain==True:
+    print("bring umbrella.")
+else:
+    print("no umbrella.")
+
+age=66
+if age >65:
+    print("senior citizen.")
+elif age > 18:
+    print("adult.")
+else:
+    print("child.")
+
+    has_key = False
+    Has_magic = True
+    if has_key or Has_magic:
+        print("You can enter.")
+    else:
+        print("You cannot enter.")
+
+#QUIZ
+ #integter
+ x=7
+ #flloat= demical
+y=3.14
+#string
+name="Steve" 
+#Boolean
+z=True
+#lists
+students=["Alice", "Bob", "Charlie", "Ruilin", 1]
+students.append("David")
+#print(students[4])
+for student in students:
+    if( student == "David"):
+        print(student, "found!")
